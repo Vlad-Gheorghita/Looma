318a2aebe7c35cc79d2e8fe7cc39a8da9a8c230a
Vlad Gheorghita <vlad.gheorghita.s@outlook.com>
2025-01-09 18:10:20 +0200

    added tab navigation + dummy screen

---
+++ src/navigation/AppNavigator.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import HomeScreen from '../screens/HomeScreen/HomeScreen';
+
+
+const Tab = createBottomTabNavigator();
+
+const TabNavigator: React.FC = () => {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default TabNavigator;
---
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const HomeScreen: React.FC = () => {
+    return (
+        <View>
+            <Text>This is the HomeScreen</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({});
+
+export default HomeScreen;
