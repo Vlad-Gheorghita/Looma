49e97ad30327850a1d2268721098cd3d30bb807e
Vlad Gheorghita <vlad.gheorghita.s@outlook.com>
2025-01-18 13:14:13 +0200

    reverted changes

--- src/services/firebase/authService.ts
+++ src/services/firebase/authService.ts
@@ -1,5 +1,4 @@
-import auth, {FirebaseAuthTypes} from "@react-native-firebase/auth"
-
+import auth, { FirebaseAuthTypes } from "@react-native-firebase/auth"
 
 export const register = (email: string, password: string) : Promise<FirebaseAuthTypes.UserCredential> => {
     return auth().createUserWithEmailAndPassword(email,password);
