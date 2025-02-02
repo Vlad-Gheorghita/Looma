63af390616533624c7ab7084793580c5eb132bdb
Vlad Gheorghita <vlad.gheorghita.s@outlook.com>
2025-02-02 18:22:28 +0200

    env variables the rest of it

---
+++ app.config.js
@@ -0,0 +1,51 @@
+const IS_DEV = process.env.APP_VARIANT === "development";
+const IS_PREVIEW = process.env.APP_VARIANT === "preview";
+
+const getAppName = () => {
+  if (IS_DEV) {
+    return "Home App Assistant (Dev)";
+  }
+
+  if (IS_PREVIEW) {
+    return "Home App Assistant (Preview)";
+  }
+
+  return "Home App Assistant";
+};
+
+export default {
+  expo: {
+    name: getAppName(),
+    slug: "Home-App-Assistant",
+    version: "0.1.1",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    newArchEnabled: true,
+    splash: {
+      image: "./assets/splash-icon.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
+    },
+    ios: {
+      supportsTablet: true,
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/app-icon.png",
+        backgroundColor: "#ffffff",
+      },
+      googleServicesFile:  process.env.GOOGLE_SERVICES_JSON ?? "./firebase/google-services.json",
+      package: "com.homeAssistant",
+    },
+    web: {
+      favicon: "./assets/favicon.png",
+    },
+    plugins: ["@react-native-firebase/app", "@react-native-firebase/auth"],
+    extra: {
+      eas: {
+        projectId: "ff1ff33a-91ec-4d4d-bf32-cab93983300d",
+      },
+    },
+  },
+};
