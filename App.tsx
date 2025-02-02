5f654dccb5959997e13314a0840463a9231d02cf
Vlad Gheorghita <vlad.gheorghita.s@outlook.com>
2025-02-02 23:34:38 +0200

    created custom toast

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import AppNavigator from "./src/navigation/AppNavigator";
 import React from "react";
 import { AuthProvider, useAuth } from "state/AuthContext";
 import LoadingOverlay from "@components/LoadingOverlay";
+import Toast from "react-native-toast-message";
 
 const AppContent = () => {
   const { authState } = useAuth(); // Access the loading state from AuthContext
@@ -21,6 +22,7 @@ export default function App() {
       <SafeAreaView style={{ flex: 1 }}>
         <AppContent />
       </SafeAreaView>
+      <Toast />
     </AuthProvider>
   );
 }
