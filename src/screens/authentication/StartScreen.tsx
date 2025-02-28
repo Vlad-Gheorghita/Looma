daba49b9f693661b34e40807815f9f32e9bbc903
Vlad Gheorghita <vlad.gheorghita.s@outlook.com>
2025-02-28 22:27:11 +0200

    refactor to use flex properly and added logo

--- src/screens/authentication/StartScreen.tsx
+++ src/screens/authentication/StartScreen.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Image } from "react-native";
 import { navigate } from "@navigationService";
 import { Button } from "@components";
 const StartScreen: React.FC = () => {
 
   return (
     <View style={styles.screenContainer}>
+      <Image source={require("@images/looma-logo_no-bg.png")} style={styles.logoStyle}/>
       <Button
         styling={{ button: styles.buttonStyle }}
         title="Log In"
@@ -26,11 +27,17 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#f8f9fa",
+    gap: 15
+
   },
   buttonStyle: {
-    width: "80%",
-    margin: "1%",
+    width: "60%",
   },
+  logoStyle: {
+    height: "30%",
+    aspectRatio: 1,
+    resizeMode: "contain"
+  }
 });
 
 export default StartScreen;
