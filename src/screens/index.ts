dcd691876372a9ef5aecffd5a7931691bb9db530
Vlad Gheorghita <vlad.gheorghita.s@outlook.com>
2025-05-11 19:45:16 +0300

    added comments

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,6 +1,8 @@
+//Authentication Section
 export { default as LoginScreen} from "./authentication/LoginScreen"
 export { default as RegisterScreen} from "./authentication/RegisterScreen"
 export { default as StartScreen} from "./authentication/StartScreen"
 
+//Application Section
 export {default as BillSplitScreen} from "./app/BillSplitScreen"
 export {default as SettingsScreen} from "./app/SettingsScreen"
