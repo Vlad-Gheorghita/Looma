399f8131f575892b98ea9eff2599da0434b77b2f
Vlad Gheorghita <62440742+Vlad-Gheorghita@users.noreply.github.com>
2025-05-12 16:53:46 +0300

    [Bill Splitting Page] OCR Configuration (#25)
    * added comments
    
    * configured and working OCR service

--- src/screens/app/BillSplitScreen.tsx
+++ src/screens/app/BillSplitScreen.tsx
@@ -1,11 +1,46 @@
 import { globalStyling } from "@styling";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { Alert, StyleSheet, Text, View } from "react-native";
+import { launchCamera, launchImageLibrary } from "react-native-image-picker";
+import { recognizeTextFromImage, parseBillText } from "@ocrService";
+import { Button } from "@components";
+
+type Item = {
+  name: string;
+  price: number;
+};
 
 const BillSplitScreen: React.FC = () => {
+  const [imageUri, setImageUri] = useState<string | null>(null);
+  const [items, setItems] = useState<Item[]>([]);
+
+  //TODO: Work in Progress
+  const handleScanBill = async () => {
+    console.log("button pressed");
+    // const result = await launchCamera({ mediaType: "photo" });
+    const result = await launchImageLibrary({mediaType: "photo"});
+    // console.log(result);
+    const uri = result.assets?.[0]?.uri;
+    if (!uri) return;
+
+    setImageUri(uri);
+    // setLoading(true);
+
+    try {
+      const text = await recognizeTextFromImage(uri);
+      console.log(text);
+      // const parsed = parseBillText(text);
+      // setItems(parsed);
+    } catch (error) {
+      Alert.alert("OCR Error", "Failed to recognize text from image.");
+    } finally {
+      // setLoading(false);
+    }
+  };
+
   return (
     <View style={[globalStyling.pageStyle]}>
-      
+      <Button title="Upload Photo" onPress={handleScanBill} />
     </View>
   );
 };
