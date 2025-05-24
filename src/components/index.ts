4f7a5896ca58403e18e5f5808bda0f7a55406f65
Vlad Gheorghita <vlad.gheorghita.s@outlook.com>
2025-05-24 16:24:07 +0300

    avatar importing

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export { default as Input } from './Input';
 export { default as LoadingOverlay } from './LoadingOverlay';
 export { default as Card } from './Card';
 export { default as AnimatedCard } from './AnimatedCard';
+export { default as Avatar } from './Avatar';
