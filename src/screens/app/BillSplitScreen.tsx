5af89215947d39b3ee7e4a79ce914834e12e6ba1
Vlad Gheorghita <vlad.gheorghita.s@outlook.com>
2025-05-24 20:26:20 +0300

    configured card popout

--- src/screens/app/BillSplitScreen.tsx
+++ src/screens/app/BillSplitScreen.tsx
@@ -15,9 +15,15 @@ import { usePulseAnimation } from "@hooks";
 import { ToastService } from "@toastService";
 import { LinearGradient } from "expo-linear-gradient";
 import EditIcon from "@icons/edit-icon.svg";
+import CloseIcon from "@icons/close-square-icon.svg";
+import { BillItem } from "../../types/billItemTypes";
 
 const BillSplitScreen: React.FC = () => {
   const [popupVisible, setPopupVisible] = useState(false);
+  const [selectedItem, setSelectedItem] = useState<BillItem | undefined>(
+    undefined
+  );
+  const [gradientColor, setGradientColor] = useState("#eeeeee")
 
   const { scale } = usePulseAnimation();
   const animatedStyle = useAnimatedStyle(() => {
@@ -40,58 +46,68 @@ const BillSplitScreen: React.FC = () => {
     }
   };
 
-  const cardData = [
+  const cardData: BillItem[] = [
     {
-      id: "1",
-      itemName: "Banane",
+      id: 1,
+      name: "Banane",
       price: "5.67",
       currency: "LEI",
     },
     {
-      id: "2",
-      itemName: "Carne",
+      id: 2,
+      name: "Carne",
       price: "27.30",
       currency: "LEI",
     },
     {
-      id: "3",
-      itemName: "Paine",
+      id: 3,
+      name: "Paine",
       price: "5.32",
       currency: "LEI",
     },
     {
-      id: "4",
-      itemName: "Lapte",
+      id: 4,
+      name: "Lapte",
       price: "9.99",
       currency: "LEI",
     },
     {
-      id: "5",
-      itemName: "Lapte",
+      id: 5,
+      name: "Lapte",
       price: "9.99",
       currency: "LEI",
     },
     {
-      id: "6",
-      itemName: "Lapte",
+      id: 6,
+      name: "Lapte",
       price: "9.99",
       currency: "LEI",
     },
     {
-      id: "7",
-      itemName: "Lapte",
+      id: 7,
+      name: "Lapte",
       price: "9.99",
       currency: "LEI",
     },
     {
-      id: "8",
-      itemName: "Lapte",
+      id: 8,
+      name: "Lapte",
       price: "9.99",
       currency: "LEI",
     },
   ];
 
-  const bgColor = "#dddddd";
+
+  const openCardPopup = (item: BillItem): void => {
+    setSelectedItem(item);
+    setPopupVisible(true);
+    setGradientColor("#bbbbbb");
+  };
+
+  const closeCardPopup = (): void => {
+    setPopupVisible(false);
+    setGradientColor("#eeeeee");
+  };
 
   return (
     <View style={[globalStyling.pageStyle]}>
@@ -107,57 +123,77 @@ const BillSplitScreen: React.FC = () => {
       </View>
 
       <View style={styles.reciptItemsListContainer}>
-        <FlatList
-          contentContainerStyle={{ gap: 10, paddingBottom: 10 }}
-          showsVerticalScrollIndicator={false}
-          data={cardData}
-          keyExtractor={(item) => item.id}
-          renderItem={({ item }) => (
-            <Card style={styles.cardStyle}>
-              <View style={styles.cardItemDetailsStyle}>
-                <Pressable onPress={() => setPopupVisible(true)}>
-                  <View style={[styles.cartItemNameContainer]}>
-                    <Text style={{ fontWeight: "bold" }}>{item.itemName}</Text>
-                    <EditIcon width={"15%"} height={"100%"} />
-                  </View>
-                </Pressable>
-                <Text style={{ fontWeight: "bold" }}>
-                  {item.price} {item.currency}
-                </Text>
-              </View>
-              <View style={styles.cardAssignedPersonsStyle}>
-                <Avatar initials="Vl" />
-                <Avatar initials="La" />
-                <Avatar initials="Da" />
-                <Avatar initials="Dr" />
-              </View>
-            </Card>
-          )}
-        />
-
-        {/* Top gradient shadow */}
-        {/* <LinearGradient
-          colors={[bgColor, bgColor + "00"]}
+        <View style={styles.reciptItemsList}>
+          <FlatList
+            contentContainerStyle={{ gap: 10, paddingBottom: 10 }}
+            showsVerticalScrollIndicator={false}
+            data={cardData}
+            keyExtractor={(item) => item.id.toString()}
+            renderItem={({ item }) => (
+              <Card style={styles.cardStyle}>
+                <View style={styles.cardItemDetailsStyle}>
+                  <Pressable onPress={() => openCardPopup(item)}>
+                    <View style={[styles.cartItemNameContainer]}>
+                      <Text style={{ fontWeight: "bold" }}>{item.name}</Text>
+                      <EditIcon width={"15%"} height={"100%"} />
+                    </View>
+                  </Pressable>
+                  <Text style={{ fontWeight: "bold" }}>
+                    {item.price} {item.currency}
+                  </Text>
+                </View>
+                <View style={styles.cardAssignedPersonsStyle}>
+                  <Avatar initials="Vl" />
+                  <Avatar initials="La" />
+                  <Avatar initials="Da" />
+                  <Avatar initials="Dr" />
+                </View>
+              </Card>
+            )}
+          />
+          {/* Top gradient shadow */}
+          <LinearGradient
+          colors={[gradientColor, gradientColor + "00"]}
           style={styles.topShadow}
           pointerEvents="none"
-        /> */}
-
-        {/* Bottom gradient shadow */}
-        <LinearGradient
-          colors={[bgColor + "00", bgColor]}
-          style={styles.bottomShadow}
-          pointerEvents="none"
         />
+
+          {/* Bottom gradient shadow */}
+          <LinearGradient
+            colors={[gradientColor + "00", gradientColor]}
+            style={styles.bottomShadow}
+            pointerEvents="none"
+          />
+        </View>
+
+        <Card style={styles.cardStyle}>
+          <View style={styles.cardTotalStyle}>
+            <Text style={{ fontWeight: "bold" }}>Total</Text>
+            <Text style={{ fontWeight: "bold" }}>127.38 LEI</Text>
+          </View>
+        </Card>
       </View>
 
-      <AnimatedPopupCard
-        visible={popupVisible}
-        onClose={() => setPopupVisible(false)}
-      >
-        <Text>Hopaaaa</Text>
-        <Pressable onPress={() => setPopupVisible(false)}>
-          <Text style={{ fontWeight: "bold" }}>Inchiiideee baaa</Text>
-        </Pressable>
+      <AnimatedPopupCard visible={popupVisible} onClose={closeCardPopup}>
+        <View style={styles.cardPopupContainer}>
+          <View style={styles.cardPopupHeader}>
+            <Text style={{ fontWeight: "bold" }}>{selectedItem?.name}</Text>
+            <View style={styles.cardPopupHeaderClose}>
+              <Text style={{ fontWeight: "bold" }}>
+                {selectedItem?.price} {selectedItem?.currency}
+              </Text>
+              <Pressable onPress={closeCardPopup}>
+                <CloseIcon width={30} height={30} />
+              </Pressable>
+            </View>
+          </View>
+          <View style={styles.cardPopupPersonsContainer}>
+            <Avatar initials="Vl" />
+            <Avatar initials="La" />
+            <Avatar initials="Da" />
+            <Avatar initials="Dr" />
+          </View>
+        </View>
       </AnimatedPopupCard>
     </View>
   );
@@ -187,17 +223,22 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     overflow: "hidden",
     height: "80%",
-    width: "80%",
+    width: "50%",
   },
 
   reciptItemsListContainer: {
-    flex: 1.5,
+    flex: 2,
     justifyContent: "flex-start",
     alignItems: "stretch",
     padding: "2%",
   },
 
+  reciptItemsList: {
+    flex: 20
+  },
+
   cardStyle: {
+    flex: 1,
     justifyContent: "flex-start",
     alignItems: "stretch",
     gap: 20,
@@ -222,12 +263,44 @@ const styles = StyleSheet.create({
     flexWrap: "wrap",
   },
 
+  cardTotalStyle: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+
+  cardPopupContainer: {
+    alignItems: "stretch",
+    padding: "2%",
+    paddingTop: 0,
+    gap: 10,
+  },
+
+  cardPopupHeader: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+
+  cardPopupHeaderClose: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 10,
+  },
+
+  cardPopupItemDetails: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+
+  cardPopupPersonsContainer: { gap: 20 },
+
   topShadow: {
     position: "absolute",
     top: 0,
     left: 0,
     right: 0,
-    height: 40,
+    height: 20,
     zIndex: 10,
   },
 
---
+++ src/types/billItemTypes.ts
@@ -0,0 +1,6 @@
+export type BillItem = {
+    id: number,
+    name: string,
+    price: string,
+    currency: string,
+}
